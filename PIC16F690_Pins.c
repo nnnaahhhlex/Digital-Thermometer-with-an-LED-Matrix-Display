316d5c6d107a45deb30856a18c636245ab120605
nnnaahhhlex <73715308+nnnaahhhlex@users.noreply.github.com>
2021-07-15 21:34:08 -0400

    Create PIC16F690_Pins.c

---
+++ PIC16F690_Pins.c
@@ -0,0 +1,18 @@
+#byte TRISC = 0x87
+#byte TRISB = 0x86
+
+#bit LATCHCLOCK_COL_BLUE = 0x7.0 // C0
+#bit CLOCK_COL_BLUE = 0x7.1 // C1
+#bit DATA_BLUE = 0x7.2 // C2
+
+#bit LATCHCLOCK_ANODE = 0x7.3 // C3
+#bit CLOCK_ANODE = 0x7.4 // C4
+#bit DATA_ANODE = 0x7.5 // C5
+
+#bit LATCHCLOCK_COL_RED = 0x6.4 // B4
+#bit CLOCK_COL_RED = 0x6.5 // B5
+#bit DATA_RED = 0x6.6 // B6
+
+#bit LATCHCLOCK_COL_GREEN = 0x7.6 // C6
+#bit CLOCK_COL_GREEN = 0x7.7 //C7
+#bit DATA_GREEN = 0x6.7 // B7
